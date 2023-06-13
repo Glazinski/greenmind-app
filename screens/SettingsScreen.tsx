17bfe171248623e70035aa19b2ab19271172c2c1
Michal Kaminski <vercety.glaz@gmail.com>
2023-06-13 22:46:23 +0200

    Translations 3, SettingsScreen improvement for the future

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -10,27 +10,33 @@ export const SettingsScreen = () => {
   const { colorScheme, setColorScheme } = useColorSchemeStore();
   const [value, setValue] = React.useState(colorScheme);
 
-  const onValueChange = (value: ColorScheme) => {
-    setValue(value);
-    setColorScheme(value);
+  const onValueChange = (newColorScheme: ColorScheme) => {
+    setValue(newColorScheme);
+    setColorScheme(newColorScheme);
   };
 
   return (
     <Layout>
-      <List.Section title="Settings">
-        <List.Accordion
-          title="Theme"
-          left={(props) => <List.Icon {...props} icon="theme-light-dark" />}
-        >
-          <RadioButton.Group
-            onValueChange={(value) => onValueChange(value as ColorScheme)}
-            value={value}
+      <List.Section title={t('settings') as string}>
+        <List.AccordionGroup>
+          <List.Accordion
+            id="1"
+            title={t('theme')}
+            left={(props) => <List.Icon {...props} icon="theme-light-dark" />}
           >
-            <RadioButton.Item value="system" label={t('use_system_settings')} />
-            <RadioButton.Item value="light" label={t('light')} />
-            <RadioButton.Item value="dark" label={t('dark')} />
-          </RadioButton.Group>
-        </List.Accordion>
+            <RadioButton.Group
+              onValueChange={(value) => onValueChange(value as ColorScheme)}
+              value={value}
+            >
+              <RadioButton.Item
+                value="system"
+                label={t('use_system_settings')}
+              />
+              <RadioButton.Item value="light" label={t('light')} />
+              <RadioButton.Item value="dark" label={t('dark')} />
+            </RadioButton.Group>
+          </List.Accordion>
+        </List.AccordionGroup>
       </List.Section>
     </Layout>
   );
